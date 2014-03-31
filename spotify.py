8f3e8d70b26bd19eb7dda926a7ea8ba199534d6e
V <vlall@users.noreply.github.com>
2014-03-31 03:27:49 -0400

    Create spotify.py

---
+++ spotify.py
@@ -0,0 +1,22 @@
+import json
+import urllib2
+
+
+'''
+Filler data below, will fix tomorrow.
+'''
+
+json_data = urllib2.urlopen('https://ws.spotify.com/search/1/track.json?q=%s' % (userName))
+data = json.load(json_data)
+json_data.close()
+
+def spotify():
+	return data['top']
+
+def spotify_artist_stats(userName):
+	return data['artist']['stats']['listeners']
+
+def spotify_playcount(userName):
+	return data['artist']['stats']['playcount']
+
+print spotify_artist_stats('cher')
