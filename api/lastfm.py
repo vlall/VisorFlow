97096e187a522d16fb54025dd16c35b20f0ff938
V <vlall@users.noreply.github.com>
2014-04-04 00:28:06 -0400

    Update lastfm.py

--- api/lastfm.py
+++ api/lastfm.py
@@ -7,7 +7,7 @@ this is for Top Artists:
 http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=e72fac875761b48cd924e29014d89686&format=json&limit=6
 '''
 
-class Lastfm(object):
+class Lastfm:
 
 	def __init__(self, name):
 		json_data = urllib2.urlopen('http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=%s&api_key=2b35547bd5675d8ecb2b911ee9901f59&format=json' % (name))
