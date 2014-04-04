2968ad0188ef8f6ab2b127a3bddf8f0d5410dbbb
V <vlall@users.noreply.github.com>
2014-04-04 00:27:35 -0400

    Update spotify.py

--- api/spotify.py
+++ api/spotify.py
@@ -1,23 +1,30 @@
 import json
 import urllib2
+from print_function import prettyprint
 
+class Spotify:
 
-'''
-Filler data below, will fix tomorrow.
-'''
+	def __init__(self, name):
+		json_data = urllib2.urlopen('https://ws.spotify.com/search/1/track.json?q=%s' % (name))
+		self.data = json.load(json_data)
+		json_data.close()  
+		self.name = name
 
-json_data = urllib2.urlopen('https://ws.spotify.com/search/1/track.json?q=%s' % (userName))
-data = json.load(json_data)
-json_data.close()
+	def display_name(self):
+		return self.name
 
-def spotify():
-	return data['top']
+	def spotify_tags(self):
+		return self.data['info']
 
-def spotify_artist_stats(userName):
-	return data['artist']['stats']['listeners']
+	def spotify_artist_stats(self):
+		return self.data['artist']['stats']['listeners']
 
-def spotify_playcount(userName):
-	return data['artist']['stats']['playcount']
-	
+	def spotify_playcount(self):
+		return self.data['artist']['stats']['playcount']
+		
 if __name__ == '__main__':
-	print spotify_artist_stats('cher')
+	artist = Spotify('cher')
+	print artist.display_name() 
+	print artist.spotify_tags()
+	prettyprint.prettyPrint(artist.data)  
+
